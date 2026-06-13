615ebc0bce3c59da9546d1c70550071a02bd43dd
Abir Saha <154303202+Abir2307@users.noreply.github.com>
2026-06-14 02:59:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,10 @@ import pandas as pd
 from agents import recommendation_agent,customer_agent,analytics_agent
 import ast
 import secrets
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///KnowBuy.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'secret_key'
 
